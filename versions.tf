d8097df1d2f22b7a36affc0fb3d960cd42cbf644
Geoffrey Claro <geoffrey.claro@jumpcloud.com>
2022-04-28 19:29:35 -0400

    updating workspace info and provider versions

--- versions.tf
+++ versions.tf
@@ -1,17 +1,17 @@
 terraform {
 
   cloud {
-    organization = "hashicorp-learn"
+    organization = "edge-sandbox"
 
     workspaces {
-      name = "learn-terraform-cloud"
+      name = "learn-terraform-cloud-vcs"
     }
   }
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.28.0"
+      version = "~> 3.30.0"
     }
   }
 
