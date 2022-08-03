ee94180c435ce9920cac4934ac93c8323174214f
chethanmkumar19 <chethanmkumar19@gmail.com>
2022-08-03 08:33:37 +0000

    Remove cloud block

--- versions.tf
+++ versions.tf
@@ -1,5 +1,5 @@
 terraform {
-
+/*
   cloud {
     organization = "hashicorp-learn"
 
@@ -7,6 +7,7 @@ terraform {
       name = "learn-terraform-cloud"
     }
   }
+*/
 
   required_providers {
     aws = {
