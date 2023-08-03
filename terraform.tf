d60a6580fa1d58d85aad1a828375d1f4a28f1141
Jason Thomas Brown <jason.brown@turtlesec.no>
2023-08-03 15:01:08 +0200

    remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "turtlesec-no"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
