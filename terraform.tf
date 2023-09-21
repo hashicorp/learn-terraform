bfd1b13523e9c2566cb036ba03ce976ac25c1f41
Niranjan Pendyala <niranaws@amazon.com>
2023-09-21 11:37:43 -0400

    change

--- terraform.tf
+++ terraform.tf
@@ -1,19 +1,9 @@
+
 terraform {
-
-  cloud {
-    organization = "organization-name"
-
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.28.0"
+      version = "~> 5.0"
     }
   }
-
-  required_version = ">= 0.14.0"
 }
