d2f1960e0db3dda25a89a82371cd0ce109a06342
Kyle Moore <kyledmoore@gmail.com>
2023-10-17 23:07:50 -0400

    Add AWS cred variables and references
    Fixes warnings and auth failures during 'apply' with 4.x provider

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
   region = var.region
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 data "aws_ami" "ubuntu" {
--- variables.tf
+++ variables.tf
@@ -13,3 +13,10 @@ variable "instance_name" {
   default     = "Provisioned by Terraform"
 }
 
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "Secret access key"
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "Secret key id"
+}
