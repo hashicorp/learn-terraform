32f885c806ce36f109d711f6cd3d4820ba8f0050
Joey Lei <6957385+leizerbeam@users.noreply.github.com>
2024-04-12 16:31:57 -0700

    Update main.tf
    add org name

--- main.tf
+++ main.tf
@@ -14,7 +14,9 @@ resource "hcp_project" "provider_test_project" {
   description = "project created by hcp provider"
 }
 
-data "hcp_organization" "provider_test_org" {}
+data "hcp_organization" "provider_test_org" {
+  name = "TFC-Unification-Test-Org-1"
+}
 
 data "tfe_team" "provider_test_tfe_team" {
   name         = hcp_group.provider_test_group.display_name
