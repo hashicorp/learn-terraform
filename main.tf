7ca4a6c4fa714e734b9d16f923d31cde09389024
Joey Lei <6957385+leizerbeam@users.noreply.github.com>
2024-04-12 18:06:50 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,22 +14,18 @@ resource "hcp_project" "provider_test_project" {
   description = "project created by hcp provider"
 }
 
-data "hcp_organization" "provider_test_org" {
-  name = "TFC-Unification-Test-Org-1"
-}
-
 data "tfe_team" "provider_test_tfe_team" {
   name         = hcp_group.provider_test_group.display_name
-  organization = data.hcp_organization.provider_test_org.name
+  organization = "TFC-Unification-Test-Org-1"
 }
 
 data "tfe_project" "provider_test_tfe_project" {
   name = hcp_project.provider_test_project.display_name
-  organization = data.hcp_organization.provider_test_org.name
+  organization = "TFC-Unification-Test-Org-1"
 }
 
 resource "tfe_team_project_access" "admin" {
   access       = "read"
-  team_id      = tfe_team.provider_test_tfe_team.id
-  project_id   = tfe_project.provider_test_tfe_project.id
+  team_id      = data.tfe_team.provider_test_tfe_team.id
+  project_id   = data.tfe_project.provider_test_tfe_project.id
 }
