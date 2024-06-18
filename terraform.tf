c6bef99e29ac185559cb93458c2c764753d1d732
Thierno SOW <thiernomadjou@gmail.com>
2024-06-18 03:09:57 -0700

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -3,6 +3,7 @@
 
 terraform {
 
+  /*
   cloud {
     organization = "SOW-CorpInternational"
 
@@ -10,6 +11,7 @@ terraform {
       name = "learn-terraform"
     }
   }
+  */
 
   required_providers {
     aws = {
